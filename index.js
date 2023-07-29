ba6e12746db5593c8f142ed1841c58982687d8d1
YliaVaininlovich <ylia.v@tut.by>
2023-07-29 22:11:24 +0300

    fix: index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true })); //парсер для обработки POST запросов
 
+let name ='', email='', message='';
+let errorName = '', errorEmail = '', errorMessage = '';
+
 //на главную страницу
 app.get('/', (req, res) => {
   res.send(generateForm());
@@ -12,47 +15,63 @@ app.get('/', (req, res) => {
 
 //обработка формы
 app.post('/submit-form', (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const message = req.body.message;
+   name = req.body.name;
+   email = req.body.email;
+  message = req.body.message;
+  
+  // Если хотя бы одно поле не заполнено, то возвращаем ошибку
+  if (!name || !email || !message) { 
+    if (!name) errorName = `Error: Не заполнено имя`; else errorName = "";
+    if (!email) errorEmail = `Error: Не заполнен адрес электронной почты`; else errorEmail = "";
+    if (!message) errorMessage = `Error: Нет комментариев`; else errorMessage = "";
+    res.send(generateForm());
 
-  if (!name || !email || !message) {
-    // Если хотя бы одно поле не заполнено, то возвращаем ошибку
-    const errorText = `<p>Error: Все поля обязательны для заполнения</p>`;
-    const genForm = generateForm(name, email, message);
-    res.send(errorText+genForm);
-  } else {
-    // Если все поля заполнены, то отображаем данные
-    res.send(`
-      <p>Name: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>Message: ${message}</p>
-    `);
   }
+  else {
+    errorName = '';
+    errorEmail = '';
+    errorMessage = '';
+    
+    res.redirect(301, `/submit-result`);
+  }
+  
 });
 
+//правильно заполненная форма
+app.get('/submit-result', (req, res) => {
+  res.send(`
+  <p>Name: ${name}</p>
+  <p>Email: ${email}</p>
+  <p>Message: ${message}</p>
+`);
+}
+);
 
-app.listen(3050, () => {
-  console.log('Server is running at 3050');
+app.listen(7280, () => {
+  console.log('Server is running at 7280');
 });
 
-function generateForm(name = '', email = '', message = '') {
+function generateForm() {
  
   return `
-    <form method="POST" action="/submit-form">
+    <form method="POST" action="/submit-form" id="my-form">
+      <p style="color:red">${errorName}</p>
       <label for="name">Name:</label>
       <input type="text" id="name" name="name" value="${name}">
-      <br><br><br>
-           
+      <br><br>
+      
+      <p style="color:red">${errorEmail}</p>
       <label for="email">Email:</label>
       <input type="email" id="email" name="email" value="${email}">
-      <br><br><br>
+      <br><br>
       
+      <p style="color:red">${errorMessage}</p>
       <label for="message">Message:</label>
       <textarea id="message" name="message">${message}</textarea>
-      <br><br><br>
+      <br><br>
 
       <button type="submit">Submit</button>
     </form>
+   
   `;
 }
